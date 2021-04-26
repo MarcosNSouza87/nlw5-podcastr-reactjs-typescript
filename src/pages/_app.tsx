42eea7751bfc009601307fdf81f1cb7cfd8b6c0e
MarcosNSouza87 <m_souza@outlook.com.br>
2021-04-26 12:52:11 -0300

    fix: adjusts player context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,20 @@ import '../styles/global.scss'
 import styles from '../styles/app.module.scss'
 import { Player } from '../components/Player'
 import { PlayerContext } from './contexts/PlayerContext'
+import { useState } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex,setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return(
-  <PlayerContext.Provider value={'Diego'}>
+  <PlayerContext.Provider value={{episodeList,currentEpisodeIndex}}>
     <div className={styles.wrapper}>
       <main>
         <Header />
